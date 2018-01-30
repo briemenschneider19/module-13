d3cacb766800ce365c8707d9e5623d476545e355
Boe Riemenschneider <33259499+briemenschneider19@users.noreply.github.com>
2018-01-30 10:12:47 -0600

    Update 13.1.1.py

--- 13.1.1.py
+++ 13.1.1.py
@@ -1,24 +1,24 @@
-import RoboPiLib as RPL
-from setup import RPL
+import RoboPiLib as RPL # from setup.py
+from setup import RPL # from basic.py
 import time
-import post_to_web as PTW
-RPL.RoboPiInit("/dev/ttyAMA0",115200)
+import post_to_web as PTW # from basic.py
+RPL.RoboPiInit("/dev/ttyAMA0",115200)# from setup.py
 
-motorL = 1
-motorR = 0
-sensor_pin = 16
+motorL = 1 # motor pin number
+motorR = 0 # motor pin number
+sensor_pin = 16 # sensor pin number
 
-RPL.servoWrite(motorR, 1000)
-RPL.servoWrite(motorL, 2000)
+RPL.servoWrite(motorR, 1000) # turns right motor on
+RPL.servoWrite(motorL, 2000) # turns left motor on
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
-while RPL.digitalRead(sensor_pin) == 1:
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
-    RPL.servoWrite(motorR, 1000)
-    RPL.servoWrite(motorL, 2000)
+while RPL.digitalRead(sensor_pin) == 1: # what terminal prints if something is in the way
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin) # referring to the 16th pin (the sensor)
+    RPL.servoWrite(motorR, 1000) # turns right motor on
+    RPL.servoWrite(motorL, 2000) # turns left motor on
     PTW.post()
 else:
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
-    RPL.servoWrite(motorR, 0)
-    RPL.servoWrite(motorL, 0)
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin) # referring to the 16th pin (the sensor)
+    RPL.servoWrite(motorR, 0) # shuts off right motor
+    RPL.servoWrite(motorL, 0) # shuts off left motor
     PTW.post()
