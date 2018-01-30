4a4c98dbf223b37e2256ab7beb1ae7b8b358b89a
Boe Riemenschneider <33259499+briemenschneider19@users.noreply.github.com>
2018-01-30 10:18:24 -0600

    Update 13.1.1.py

--- 13.1.1.py
+++ 13.1.1.py
@@ -1,6 +1,6 @@
 import RoboPiLib as RPL # from setup.py
 from setup import RPL # from basic.py
-import time
+import time # from RoboPiLib.py
 import post_to_web as PTW # from basic.py
 RPL.RoboPiInit("/dev/ttyAMA0",115200)# from setup.py
 
@@ -10,15 +10,15 @@ sensor_pin = 16 # sensor pin number
 
 RPL.servoWrite(motorR, 1000) # turns right motor on
 RPL.servoWrite(motorL, 2000) # turns left motor on
-RPL.pinMode(sensor_pin,RPL.INPUT)
+RPL.pinMode(sensor_pin,RPL.INPUT) # from basic.py
 
 while RPL.digitalRead(sensor_pin) == 1: # what terminal prints if something is in the way
     PTW.state['d1'] = RPL.digitalRead(sensor_pin) # referring to the 16th pin (the sensor)
     RPL.servoWrite(motorR, 1000) # turns right motor on
     RPL.servoWrite(motorL, 2000) # turns left motor on
-    PTW.post()
+    PTW.post() # from basic.py
 else:
     PTW.state['d1'] = RPL.digitalRead(sensor_pin) # referring to the 16th pin (the sensor)
     RPL.servoWrite(motorR, 0) # shuts off right motor
     RPL.servoWrite(motorL, 0) # shuts off left motor
-    PTW.post()
+    PTW.post() # from basic.py
