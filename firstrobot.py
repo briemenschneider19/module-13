edddf198a756b7257578e769896d8859bb7729f9
Boe Riemenschneider <33259499+briemenschneider19@users.noreply.github.com>
2018-02-14 11:31:07 -0600

    Update firstrobot.py

--- firstrobot.py
+++ firstrobot.py
@@ -1,21 +1,24 @@
 import RoboPiLib as RPL
 from setup import RPL
 import time
+import post_to_web as PTW
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 motorL = 1
 motorR = 0
 sensor_pin = 16
 
-RPL.servoWrite(motorR, 2000)
-RPL.servoWrite(motorL, 1000)
+RPL.servoWrite(motorR, 1000)
+RPL.servoWrite(motorL, 2000)
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
 while RPL.digitalRead(sensor_pin) == 1:
     PTW.state['d1'] = RPL.digitalRead(sensor_pin)
-    RPL.servoWrite(motorR, 2000)
-    RPL.servoWrite(motorL, 1000)
+    RPL.servoWrite(motorR, 1000)
+    RPL.servoWrite(motorL, 2000)
+    PTW.post()
 else:
     PTW.state['d1'] = RPL.digitalRead(sensor_pin)
     RPL.servoWrite(motorR, 0)
     RPL.servoWrite(motorL, 0)
+    PTW.post()
