a166f607cd20abe7ccca31f799a1edd6615e5b1a
Boe Riemenschneider <33259499+briemenschneider19@users.noreply.github.com>
2018-03-19 10:15:06 -0500

    Update firstrobot.py

--- firstrobot.py
+++ firstrobot.py
@@ -1,24 +1,24 @@
-import RoboPiLib as RPL
-from setup import RPL
-import time
-import post_to_web as PTW
-RPL.RoboPiInit("/dev/ttyAMA0",115200)
+import RoboPiLib as RPL # (robotonomy) setup.py
+from setup import RPL # (robotonomy) readme
+import time # (robotonomy) post_to_web.py
+import post_to_web as PTW # (robotonomy) basic.py
+RPL.RoboPiInit("/dev/ttyAMA0",115200) # (robotonomy) setup.py
 
-motorL = 1
-motorR = 0
-sensor_pin = 16
+motorL = 1 # (robo-control control.py)
+motorR = 0 # (robo-control control.py)
+sensor_pin = 16 # (robotonomy basic.py)
 
-RPL.servoWrite(motorR, 2000)
-RPL.servoWrite(motorL, 1000)
-RPL.pinMode(sensor_pin,RPL.INPUT)
+RPL.servoWrite(motorR, 2000) # (robo-control control.py)
+RPL.servoWrite(motorL, 1000) # (robo-control control.py)
+RPL.pinMode(sensor_pin,RPL.INPUT) # (robo-control control.py)
 
-while RPL.digitalRead(sensor_pin) == 1:
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
-    RPL.servoWrite(motorR, 2000)
-    RPL.servoWrite(motorL, 1000)
-    PTW.post()
+while RPL.digitalRead(sensor_pin) == 1: # (robotonomy basic.py)
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin) # (robotonomy basic.py)
+    RPL.servoWrite(motorR, 2000) # (robo-control control.py)
+    RPL.servoWrite(motorL, 1000) # (robo-control control.py)
+    PTW.post() # (robotonomy basic.py)
 else:
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
-    RPL.servoWrite(motorR, 0)
-    RPL.servoWrite(motorL, 0)
-    PTW.post()
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin) # (robotonomy basic.py)
+    RPL.servoWrite(motorR, 0) # (robo-control control.py)
+    RPL.servoWrite(motorL, 0) # (robo-control control.py)
+    PTW.post() # (robotonomy basic.py)
