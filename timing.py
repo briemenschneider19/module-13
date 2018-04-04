4e395508bef1664d04e279874ff9f5431c0dd0e3
Boe Riemenschneider <33259499+briemenschneider19@users.noreply.github.com>
2018-04-04 09:45:34 -0500

    Update timing.py

--- timing.py
+++ timing.py
@@ -1,22 +1,22 @@
-import RoboPiLib as RPL
-from setup import RPL
-import time
-import post_to_web as PTW
-RPL.RoboPiInit("/dev/ttyAMA0",115200)
+import RoboPiLib as RPL # (robotonomy setup.py)
+from setup import RPL # (robotonomy README.md)
+import time # (robotonomy post_to_web.py)
+import post_to_web as PTW # (robotonomy basic.py)
+RPL.RoboPiInit("/dev/ttyAMA0",115200) # (robotonomy setup.py)
 
-motorL = 1
-motorR = 0
-t_ime = time.time()
+motorL = 1 # (robo-control control.py)
+motorR = 0 # (robo-control control.py)
+t_ime = time.time() # (robotonomy post_to_web.py)
 i = 3
 e = 6
 
 while True:
-    while time.time() < t_ime + i:
-        RPL.servoWrite(motorR, 1000)
-        RPL.servoWrite(motorL, 2000)
-    while time.time() > t_ime + i and time.time() < t_ime + e:
-        RPL.servoWrite(motorR, 0)
-        RPL.servoWrite(motorL, 0)
-    while time.time() > t_ime + e:
+    while time.time() < t_ime + i: # (robotonomy post_to_web.py)
+        RPL.servoWrite(motorR, 1000) # (robo-control control.py)
+        RPL.servoWrite(motorL, 2000) # (robo-control control.py)
+    while time.time() > t_ime + i and time.time() < t_ime + e: # (robotonomy post_to_web.py) 
+        RPL.servoWrite(motorR, 0) # (robo-control control.py)
+        RPL.servoWrite(motorL, 0) # (robo-control control.py)
+    while time.time() > t_ime + e: # (robotonomy post_to_web.py)
         i = i + 3
         e = e + 3
