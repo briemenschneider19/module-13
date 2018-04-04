d919a1a1c7a8b2e1d4a3189c1d5f3ef073c64ff1
Boe Riemenschneider <33259499+briemenschneider19@users.noreply.github.com>
2018-04-04 10:22:19 -0500

    Update timing.py

--- timing.py
+++ timing.py
@@ -6,16 +6,16 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200) # (robotonomy setup.py)
 motorL = 1 # (robo-control control.py)
 motorR = 0 # (robo-control control.py)
 t_ime = time.time() # (robotonomy post_to_web.py)
-i = 3
-e = 6
+a = 3
+b = 6
 
 while True:
-    while time.time() < t_ime + i: # (robotonomy post_to_web.py)
+    while time.time() < t_ime + a: # (robotonomy post_to_web.py)
         RPL.servoWrite(motorR, 1000) # (robo-control control.py)
         RPL.servoWrite(motorL, 2000) # (robo-control control.py)
-    while time.time() > t_ime + i and time.time() < t_ime + e: # (robotonomy post_to_web.py) 
+    while time.time() > t_ime + a and time.time() < t_ime + b: # (robotonomy post_to_web.py) 
         RPL.servoWrite(motorR, 0) # (robo-control control.py)
         RPL.servoWrite(motorL, 0) # (robo-control control.py)
-    while time.time() > t_ime + e: # (robotonomy post_to_web.py)
-        i = i + 3
-        e = e + 3
+    while time.time() > t_ime + b: # (robotonomy post_to_web.py)
+        a = a + 3
+        b = b + 3
