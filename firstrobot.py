1876e9c59586ab71a6588218f4583f1aef3eeb30
Boe Riemenschneider <33259499+briemenschneider19@users.noreply.github.com>
2018-04-04 10:09:00 -0500

    Update firstrobot.py

--- firstrobot.py
+++ firstrobot.py
@@ -12,10 +12,8 @@ RPL.servoWrite(motorL, 1000) # (robo-control control.py)
 RPL.pinMode(sensor_pin,RPL.INPUT) # (robo-control control.py)
 
 while RPL.digitalRead(sensor_pin) == 1: # (robotonomy basic.py)
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin) # (robotonomy basic.py)
     RPL.servoWrite(motorR, 2000) # (robo-control control.py)
     RPL.servoWrite(motorL, 1000) # (robo-control control.py)
 else:
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin) # (robotonomy basic.py)
     RPL.servoWrite(motorR, 0) # (robo-control control.py)
     RPL.servoWrite(motorL, 0) # (robo-control control.py)
